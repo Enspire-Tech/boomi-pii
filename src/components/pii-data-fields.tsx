fb0b03121f67e696886bc72175d76b6a220441de
Bryan Williams <bryan.williams@enspire-tech.com>
2021-05-03 12:02:31 -0400

    Ready for MVP Demo

--- src/components/pii-data-fields.tsx
+++ src/components/pii-data-fields.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { useState } from "react";
-import { IObjectData, IColumnObject } from "../interfaces/IMWData";
+import { IColumnObject } from "../interfaces/IMWData";
 import IComponentProps from "../interfaces/IComponentProps";
 import { Pager } from "./pager";
-// import IComponentProps from "../interfaces/IComponentProps";
+import {v4 as uuidv4} from "uuid";
 
 require("../styles/pii-data-fields.css");
 
@@ -21,133 +21,142 @@ interface IPIIData {
 export const PIIDataFields = 
     (props: IComponentProps) => {
 
-        const [currentPage, setCurrentPage] = useState(1);
-        const [searchTerms, setSearchTerms] = useState<string[]>([""]);
-        // const [sortColumn, setSortColumn] = useState("");
-        const [pageSize, setPageSize] = useState(10);
+    const uuid = uuidv4();
 
-        /*
-        state = {
-            accountId: "",
-            columns: [],
-            values: [],
-            filteredValues: [],
-            pageSize: 10,
-            currentPage: 1,
-            searchTerms: [""],
-            sortColumn: ""
-        };
-        */
+    let headerLabel: any;
+
+    //  currentPage ================================================================
+    const [currentPage, setCurrentPage] = useState(1);
     
-    
-    const getData = (): IPIIData => {
-
-        // get data from the engine
-        const model = manywho.model.getComponent(props.id, props.flowKey);
-        console.log("model", model);
-                
-       let searchTerms: string[] = [];
-       let data: IPIIData = {
-            accountId: "",
-            columns: [],
-            values: [],
-            numberOfRecords: 0
-        };
-
-        // get the columns to display
-        model.columns.map((col: IColumnObject) => 
-            {
-                data.columns.push({label: col.label, developerName: col.developerName});
-                searchTerms.push("");
-            }
-        );
-        
-        if (model.objectData === null) return {
-                                            accountId: "",
-                                            columns: [{label: "", developerName: ""}],
-                                            values: [],
-                                            numberOfRecords: 0
-                                        };
-
-        data.numberOfRecords = model.objectData.length;
-        // get the fields we're displaying 
-        model.objectData.forEach((od: IObjectData, i: number) => { 
-
-            if (i + 1 < (currentPage - 1) * pageSize || i + 1 > currentPage * pageSize) return;
-            
-            let searchResult = true;
-            /*
-            searchTerms.forEach((term, i) => {
-                // if(typeof od.properties[i].children === "string" && 
-                if (!od.properties[i].contentValue.toLowerCase().includes(term.toLowerCase())) {
-                    searchResult = false;
-                }
-            });
-            */
-            
-            if ( !searchResult ) return;
-            
-            const vals: JSX.Element[] = [];
-            data.columns.forEach(col => {
-                const field = <span>{manywho.utils.getObjectDataProperty(od.properties, col.developerName).contentValue}</span>;
-                vals.push(field);
-            });
-            const field = <button className="btn fiter">
-                                <a href={manywho.utils.getObjectDataProperty(od.properties, "Profile Link").contentValue} target="_blank">
-                                    View Profile
-                                </a>
-                            </button>;
-            vals.push(field);
-            data.values.push(vals);
-            
-        });
-
-        return data;
-    };
-
     const onPageChange = (newPage: number) => {
         setCurrentPage(newPage);
     };
 
-    const toggleFilter = () => {
-        const el = document.querySelector("div.filter-row");
+    //  searchTerms ================================================================
+    const [searchTerms, setSearchTerms] = useState<string[]>([]);
+
+    const updateSearchTerms = ((e: any) => {
+        if(!(e.currentTarget.value || e.currentTarget.value.length === 1)) return;
+        searchTerms[parseInt(e.currentTarget.dataset.index)] = e.currentTarget.value;
+        
+        setSearchTerms([...searchTerms]);
+        setCurrentPage (1);
+    });
+    
+    //  pageSize ================================================================
+    const [pageSize, ] = useState(10);
+
+    //  sortColumn ================================================================
+    // const [sortColumn, setSortColumn] = useState("");
+
+    //  ===========================================================================
+
+    const toggleFilter = (e: any) => {
+        const el = document.getElementById(uuid); 
+        el?.querySelector("input")?.focus();
         el?.classList.toggle("hidden");
         el?.classList.toggle("fade-in");
     };
+    
+    // get data from the engine ===================================================
+    const model = manywho.model.getComponent(props.id, props.flowKey);
+    // console.log("model", model);
 
-    const updateSearchTerms = ((e: any) => {
+    let data: IPIIData = {
+        accountId: "",
+        columns: [],
+        values: [],
+        numberOfRecords: 0
+    };
+
+    // get the columns to display
+    model.columns.map((col: IColumnObject) => 
+        {
+            data.columns.push({label: col.label, developerName: col.developerName});
+            // ssearchTerms.push("");
+        }
+    );
+    
+    if (model.objectData !== null) {
+
+        let pageRows = 0;
         
-        searchTerms[parseInt(e.currentTarget.dataset.index)] = e.currentTarget.value;
+        // get the data to render
+        // use a for loop because the callback in forEach is async and UI updates promises are fulfilled
+        for (var i = 0; i < model.objectData.length; i++) {
 
-        /*
-        let filteredValues: JSX.Element[][] = [];
-        vals.forEach((val, i) => {
+            const od = model.objectData[i];
+            
             let searchResult = true;
-            state.searchTerms.forEach((term, i) => {
-                if(typeof val[i].props.children === "string" && 
-                !val[i].props.children.toLowerCase().includes(term.toLowerCase())) {
+            
+            searchTerms.forEach((term, i) => {
+                if (!manywho.utils.getObjectDataProperty(od.properties, data.columns[i].developerName).contentValue.toLowerCase().includes(term.toLowerCase())) {
                     searchResult = false;
                 }
             });
-            if ( searchResult ) filteredValues.push(val);
-        });
-        */
-        setCurrentPage (1);
-        setSearchTerms([...searchTerms]);
+            
+            if ( searchResult ) {
+                
+                data.numberOfRecords ++;
+                
+                if (data.numberOfRecords > (currentPage - 1) * pageSize && data.numberOfRecords <= currentPage * pageSize) {
+                    
+                    pageRows ++;
 
-        // setData(data);
-        
-    });
+                    if (!(pageRows > pageSize)) {
 
-    let data = getData();
-    
+                        const vals: JSX.Element[] = [];
+                        data.columns.forEach(col => {
+                            const field = <span>{manywho.utils.getObjectDataProperty(od.properties, col.developerName).contentValue}</span>;
+                            vals.push(field);
+                        });
+                        const link = <button className="btn fiter">
+                                            <a href={manywho.utils.getObjectDataProperty(od.properties, "Profile Link").contentValue} target="_blank">
+                                                View Profile
+                                            </a>
+                                        </button>;
+                        vals.push(link);
+                        data.values.push(vals);
+                    }
+                }
+            }
+        };
+    }
+
     const colGridWidth = data.columns ? Math.floor(12 / (data.columns.length + 1)) : 1;
     const classes = `padded-small center-vertical col-xs-${(colGridWidth).toString()}`;
-    /*
-    const startIdx = (pageSize * currentPage) - pageSize;
-    const endIdx = (pageSize * currentPage) - 1;
-    */
+    const pages = Math.ceil(data.numberOfRecords / pageSize);
+
+    headerLabel = <h4>
+        {model.label}
+        {data.numberOfRecords > 0 && 
+            <span className="small-text">&nbsp;({data.numberOfRecords})</span>
+        }
+    </h4>;
+
+    if (data.numberOfRecords === 0 ) {
+        return <div  className="container-fluid pii-data-fields">
+                    {headerLabel}
+                    <div className="row title-row">
+                        {
+                            data.columns.map(col => {
+                                return <div className={classes}>
+                                    {col.label}
+                                </div>; 
+                            })
+                        }
+                        <div className={classes}>
+                            Profile
+                        </div>
+                    </div>
+                    <div className="row">
+                        No fields found
+                    </div>
+                </div>
+    }
+
     return <div  className="container-fluid pii-data-fields">
+        {headerLabel}
         <div className="row title-row">
             {
                 data.columns.map(col => {
@@ -164,7 +173,7 @@ export const PIIDataFields =
                 Profile
             </div>
         </div>
-        <div className="row filter-row hidden">
+        <div className="row filter-row hidden" id={uuid}>
             {
                 data.columns.map((col, i) => {
                     return <div className={classes}>
@@ -181,25 +190,22 @@ export const PIIDataFields =
         <div className="row">
             {
                 data.values.map((value, i) => {
-                    //if (i >= startIdx && i <= endIdx) {
-                        return value.map((val, i) => {
-                            return <div className={classes}>
-                                {val}
-                            </div>;
-                        });
-                    //}
+                    return value.map((val, i) => {
+                        return <div className={classes}>
+                            {val}
+                        </div>;
+                    });
                 })
             }
         </div>
-
+        {pages > 1 && 
         <Pager
             pageSize = {pageSize}
             currentPage = {currentPage}
-            pages = {Math.ceil(data.numberOfRecords / pageSize)}
+            pages = {pages}
             onPageChange = {onPageChange}
-        />
+        />}
     </div>;
-
 };
 
 
